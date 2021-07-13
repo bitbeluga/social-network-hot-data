d97fd1a384dea1b8ec044dd281ee13a0a3f198c9
Belouga.E <wangjingnic1993@gmail.com>
2021-07-13 20:28:58 +0800

    Requests

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+# print('Hello World!')
+#
+# weekdays = "Little Robert asked his mother for two cents.\
+#      'What did you do with the money I gave you yesterday?'"
+# print(weekdays)
+#
+# print("Who are you?")
+# you = input()
+# print("Hello!")
+# print(you)
+
+# import keyword
+# print(keyword.kwlist)
+import requests
+url='https://s.weibo.com/top/summary/summary?cate=realtimehot'
+strhtml=requests.get(url)
+print(strhtml.text)
