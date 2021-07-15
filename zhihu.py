11c9a2f7223282b4b243d6bfaaab0547c9fced63
Belouga.E <wangjingnic1993@gmail.com>
2021-07-15 21:25:07 +0800

    zhihu

---
+++ zhihu.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.zhihu.com/billboard'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "lxml")
+
+data = soup.select(
+    '#root > div > main > div > a > div.HotList-itemBody')
+for item in data:
+    result = {
+        'title': item.select('.HotList-itemTitle')[0].get_text(),
+        'hotCount': item.select('.HotList-itemMetrics')[0].get_text(),
+    }
+    print(result)
