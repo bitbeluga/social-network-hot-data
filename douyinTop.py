d856ed886afbab66f4dc46bd78f789a6668885ef
Belouga.E <wangjingnic1993@gmail.com>
2021-07-15 20:55:07 +0800

    douyinTop

--- douyinTop.py
+++ douyinTop.py
@@ -1,15 +1,11 @@
-import json
 from bs4 import BeautifulSoup
 import requests
-import re
 
-url = 'https://d.weibo.com/231650'
+url = 'https://tophub.today/n/K7GdaMgdQy'
 headers = {
-    'Postman-Token': '',
-    'Host': '',
-}
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
 response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(response.text)
 data = soup.select(
     '#page > div.c-d.c-d-e > div.Zd-p-Sc > div:nth-child(1) > div.cc-dc-c > div > div.jc-c > table > tbody > tr > td.al > a')
 for item in data:
