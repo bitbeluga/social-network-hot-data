1dc9c22cea1dd24d29942c33f31beae041c564f4
Belouga.E <wangjingnic1993@gmail.com>
2021-07-20 14:47:25 +0800

    zhihu

--- zhihu.py
+++ zhihu.py
@@ -1,22 +1,59 @@
-from bs4 import BeautifulSoup
-import requests
-from datetime import datetime
+# from bs4 import BeautifulSoup
+# import requests
+# from datetime import datetime
+#
+# print("\n""\n""Zhihu start_at:", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+#
+# url = 'https://www.zhihu.com/billboard'
+# headers = {
+#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+# response = requests.get(url, headers=headers)
+# soup = BeautifulSoup(response.text, "lxml")
+# print(soup)
+#
+# data = soup.select(
+#     '#root > div > main > div > a')
 
-print("\n""\n""Zhihu start_at:", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+# for item in data:
+# result = {
+#     'rank': item.select('div.HotList-itemPre > .HotList-itemIndex')[0].get_text(),
+#     'count': item.select('div.HotList-itemBody > .HotList-itemMetrics')[0].get_text(),
+#     'title': item.select('div.HotList-itemBody > .HotList-itemTitle')[0].get_text(),
+# }
+# print(result)
+
+# print("Zhihu end_at:", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+
+
+import re
+import requests
+import datetime
+import pandas as pd
+
+Domain_Name = 'https:'
+headers = {
+    'Referer': "https://www.zhihu.com/billboard",
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+}
 
 url = 'https://www.zhihu.com/billboard'
-headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+
 response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "lxml")
 
-data = soup.select(
-    '#root > div > main > div > a > div.HotList-itemBody')
-for item in data:
-    result = {
-        'title': item.select('.HotList-itemTitle')[0].get_text(),
-        'hotCount': item.select('.HotList-itemMetrics')[0].get_text(),
-    }
-    print(result)
-
-print("Zhihu end_at:", datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+html = response.text
+# print('----------------???----------------')
+# ???????????
+content = re.findall(r'<div class="HotList-itemTitle">([\s\S]+?)</div>', html, re.M)  # ??????
+hot = re.findall(r'<div class="HotList-itemMetrics">([\s\S]+?)</div>', html, re.M)  # ??????
+url = re.findall(r'"link":{"url":"([\s\S]+?)"}},', html, re.M)  # ???????
+describe = re.findall(r'"excerptArea":{"text":"([\s\S]+?)"},', html, re.M)  # ???????
+dts = []
+rank = 0
+for i in range(len(content)):
+    rank = rank + 1
+    lst = [content[i], rank, hot[i], str(url[i]).replace('u002F', '')]
+    dts.append(lst)
+df = pd.DataFrame(dts, columns=['name', 'rank', 'hot', 'url'])
+df.to_excel('./zhihu' + str(datetime.datetime.now().strftime('%Y%m%d')) + '.xlsx',
+            encoding='gbk')  # ??excel?
+# print('????')
